b69cf2e178025f747a271fbb299bd24d550f66b6
Fedulov Danil <danya7777kg@gmail.com>
2026-01-14 22:30:08 +0600

    Подгрузил все необходимые библиотеки, также написал правило для хуков в eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import './App.css';
+import {Container} from '@mui/material';
+import {Route, Routes} from 'react-router-dom';
+
 
 const App = () => {
 
 
   return (
     <>
+
+        <Container>
+            <Routes>
+                <Route path="/" element={(<App />)} />
+            </Routes>
+        </Container>
 
     </>
   )
